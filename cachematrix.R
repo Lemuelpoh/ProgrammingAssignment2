60e182e5122f23286a8d637bbedec230ce5bb463
Lemuelpoh <lemuelpoh@hotmail.com>
2019-05-23 10:17:26 +0800

    test
    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Matrix inverse: If a matrix is denoted A, we can assume its inverse is B <- A^(-1)
+## If we take A*B we get the Identity Matrix
+## A*B = B*A = I (This should be true) essentially its matrice division
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  I <- NULL                                #following the given example mean of a vector
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  get <- function() x
+  setINV <- function(inverse) I <<- inverse
+  getINV <- function() I
+  list(set = set, get = get,
+       setINV = setINV,
+       getINV = getINV)
+  
 }
 
 
-## Write a short comment describing this function
+## cache solve is to obtain the inverse of the result of makecachematirx
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  I <- x$getINV()
+  if(!is.null(I)) {
+    message("getting cached data")
+    return(I)
+  }
+  data <- x$get()
+  I <- solve(data, ...)
+  x$setINV(I)
+  I
 }
